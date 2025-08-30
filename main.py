83afb6d08b3c520cae62e5c79bdf1c950bc0f27d
Kyu Lee <kwooler@gmail.com>
2025-08-30 00:33:51 -0700

    update your repo

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 def main():
-    print('Hello World')
-    print('CS110: Introduction to Programming Concepts and Methodologies')
+    """
+    ##################################################
+    Comlete your code here
+    ret1 = 'Hello World!'
+    ret2 = 'CS V11 Programming Fundamentals'
+    print(ret1)
+    print(ret2)
+    ##################################################
+    """
+    ret1 = 'Hello World!'
+    ret2 = 'CS V11 Programming Fundamentals'
+    print(ret1)
+    print(ret2)
+
+    """
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    """
+    return ret1, ret2
+
 
 if __name__ == '__main__':
     main()
--- main_test.py
+++ main_test.py
@@ -1,1 +1,41 @@
+import pytest
+import main
+import io
+import sys
+import re
 
+
+@pytest.mark.basic
+def test_main_1():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    # datastr = '40 \n 40 \n 20'
+    # sys.stdin = io.StringIO(datastr)
+
+    ret1, ret2 = main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    res = re.search(r'[\w,\W]*[h,H]ello[\w,\W]*[w,W]orld', ret1)
+    print(res.group())
+    assert res is not None, 'Expect Hello World!'
+
+
+@pytest.mark.edge
+def test_main_2():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    # datastr = '40 \n 40 \n 20'
+    # sys.stdin = io.StringIO(datastr)
+
+    ret1, ret2 = main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    res = re.search(r'[\w,\W]*CS[\w,\W]*V11', ret2)
+    print(res.group())
+    assert res is not None, 'Expect Hello World!'
